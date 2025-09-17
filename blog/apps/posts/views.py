b2b860e0d319ed5a5ad6fd715cba51be7d3a8fab
GabiPoko <gabipokorasky@gmail.com>
2025-09-17 09:46:08 -0300

    Actualizando views, url y template para posts

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
+#Vista basada en clases
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/posts.html"
+    context_object_name = "posts"
 
-# Create your views here.
 
-def posts (request):
-    posts= Post.objects.all()
-    return render(request, 'posts/posts.html', {'posts':posts})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/post_individual.html"
+    context_object_name = "posts"
+    pk_url_kwarg = "id"
+    queryset = Post.objects.all()
